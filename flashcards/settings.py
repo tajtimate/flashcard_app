86132447c29dc39438c973ad95379e78ca913b6a
tajtimate <mate.tajti.dev@gmail.com>
2022-08-31 09:44:34 +0200

    Hiding django sec key properly

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$(1qj6o)xb!soprf0cnf#u&5l(c&88w@5dl0no4rdbgw_5b_!'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
